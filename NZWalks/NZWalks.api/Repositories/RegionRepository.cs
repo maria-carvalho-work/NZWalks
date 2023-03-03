ef335defcce4b51be001b0522e78b88c916c3795
maria-carvalho-work <mia.e.ca@gmail.com>
2023-03-03 15:01:35 +0200

    End of Section 3

---
+++ NZWalks/NZWalks.api/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.api.Data;
+using NZWalks.api.Models.Domain;
+
+namespace NZWalks.api.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private NZWalksDbContext nZWalksDbContext;
+        public RegionRepository(NZWalksDbContext nZWalksDbContext)
+        {
+            this.nZWalksDbContext = nZWalksDbContext;
+        }
+
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+            return await nZWalksDbContext.Regions.ToListAsync();
+        }
+    }
+}
